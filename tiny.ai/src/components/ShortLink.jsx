1d30e8a617b06dedb6e7695c736aca516b3e29b1
Gantavya Saraswat <gantavya99@gmail.com>
2022-10-05 23:49:11 +0530

    Temporarily removed copy to clipboard

--- tiny.ai/src/components/ShortLink.jsx
+++ tiny.ai/src/components/ShortLink.jsx
@@ -1,22 +1,11 @@
-import React from 'react';
-import { useState } from 'react';
-import CopyToClipboard from "react-copy-to-clipboard";
+import React from 'react'
+
 const ShortLink = () => {
-  const[shortenLink,setShortenLink]=useState("Hello world");
-  const[copied,setCopied]=useState(false);
-  useEffect(()=>{
-    const timer = setTimeOut
-  })
   return (
     <div className="Short-link">
           
-    <p className="shorten-link">{shortenLink} </p>
-    <CopyToClipboard text={shortenLink}
-    onCopy={()=>setCopied(true)}
-    >
+    <input type="text" className="shorten-link" placeholder='Copy your Tiny URL from here !'/>
     <button className="copy-btn">COPY</button>
-    </CopyToClipboard>
-    
     </div>
   )
 }
