789e7bb077ee5ef3d8731d84ef657cfa21d6afbd
Gantavya Saraswat <gantavya99@gmail.com>
2022-10-06 11:00:29 +0530

    Added fetching data when button is pressed

--- tiny.ai/src/components/Link.jsx
+++ tiny.ai/src/components/Link.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-
+import { useState } from 'react';
 const Link = () => {
+    const [link,setLink]=useState("Paste your link here !");
+    const [print,setPrint]=useState(false);
+    function getData(val){
+      setLink(val.target.value);
+    }
+    console.log(print?link:null)
+   
   return (
     <div className="link-container">
-    
-    
-    <input type="text" className="input-link" placeholder='Paste your Link Here!'/>
-    <button className="btn">Submit</button>
-   
-  
+    <input type="text" className="input-link" 
+     placeholder={link}
+     onChange={getData}/>
+    <button className="btn"
+    onClick={()=>setPrint(true)}
+    >Submit
+    </button>
      </div>
   )
 }
--- tiny.ai/src/components/ShortLink.jsx
+++ tiny.ai/src/components/ShortLink.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Link from './Link'
 const ShortLink = () => {
   return (
     <div className="Short-link">
           
-    <input type="text" className="shorten-link" placeholder='Copy your Tiny URL from here !'/>
+    <input type="text" className="shorten-link" 
+    placeholder="Your tiny link will be displayed here"/>
     <button className="copy-btn">COPY</button>
     </div>
   )
